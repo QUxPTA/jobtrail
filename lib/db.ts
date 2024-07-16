63cdc87eb273cf0a8a12f449d29e08152302d71a
kibuchijw <kibuchijw@gmail.com>
2024-07-16 23:04:41 +0300

    Refactor DB config: Made the configuration leaner, easier to understand

--- lib/db.ts
+++ lib/db.ts
@@ -1,48 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI: string = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-interface MongooseCache {
-  conn: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-}
-
-declare global {
-  namespace NodeJS {
-    interface Global {
-      mongoose: MongooseCache;
-    }
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI!)
+    console.log('Successfully connected to MongoDB')
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
   }
-}
+};
 
-const globalAny: any = global;
-
-let cached: MongooseCache = globalAny.mongoose;
-
-if (!cached) {
-  cached = globalAny.mongoose = { conn: null, promise: null };
-}
-
-async function connectToDatabase(): Promise<typeof mongoose> {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-export default connectToDatabase;
+export default connectToDatabase
