aa3437e0cb2a58190e3bc9876a5d8ef8fe068979
kibuchijw <kibuchijw@gmail.com>
2024-07-18 09:24:38 +0300

    Update: add path aliases for private resources

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => [
+    {
+      source: '/dashboard/:path*',
+      destination: '/private/dashboard/:path*',
+    },
+    {
+      source: '/profile/:path*',
+      destination: '/private/profile/:path*',
+    },
+    {
+      source: '/applications/:path*',
+      destination: '/private/applications/:path*',
+    },
+  ],
+};
 
 export default nextConfig;
