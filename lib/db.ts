af0413f8e0464e0cd7c39b2fb9943b0125ad6490
kibuchijw <kibuchijw@gmail.com>
2024-07-18 19:12:54 +0300

    Update DB config: check whether mongo is already connected to avoid duplicate connections.

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
+const connection: { isConnected?: number; } = {};
+
 const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI!)
-    console.log('Successfully connected to MongoDB')
-  } catch (error: any) {
-    console.error(`Error: ${error.message}`)
-    process.exit(1)
+  if (connection.isConnected) {
+    return;
   }
+  const db = await mongoose.connect(process.env.MONGODB_URI!);
+  connection.isConnected = db.connections[0].readyState;
+  console.log('Successfully connected to MongoDB');
 };
 
-export default connectToDatabase
+export default connectToDatabase;
